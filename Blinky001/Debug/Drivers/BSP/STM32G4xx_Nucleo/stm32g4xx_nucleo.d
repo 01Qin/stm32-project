5f37a615f440c34a10fa4659ff249b16c2f6876b
01Qin <sisukasqin@hotmail.com>
2026-05-19 17:17:58 +0300

    Add files

---
+++ Blinky001/Debug/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.d
@@ -0,0 +1,66 @@
+Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.o: \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.c \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo_errno.h \
+ ../Core/Inc/stm32g4xx_nucleo_conf.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
+ ../Core/Inc/stm32g4xx_hal_conf.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g431xx.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h \
+ C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo_errno.h:
+../Core/Inc/stm32g4xx_nucleo_conf.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
+../Core/Inc/stm32g4xx_hal_conf.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g431xx.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h:
+C:/Users/qin.wang_kuori/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.2/Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h:
