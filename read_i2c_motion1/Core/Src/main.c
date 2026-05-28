da2b76d7b8f761b1975577db336cb120c3a61ffe
01Qin <sisukasqin@hotmail.com>
2026-05-28 10:23:34 +0300

    Clean code

--- read_i2c_motion1/Core/Src/main.c
+++ read_i2c_motion1/Core/Src/main.c
@@ -57,10 +57,12 @@ HAL_StatusTypeDef status;
 
 
 /* USER CODE BEGIN PV */
-// static const uint8_t HUM_ADDR = 0x45 << 1;
-//static const uint8_t PRESS_ADDR = 0x19 << 1;
+// static const uint8_t _ADDR = 0x45 << 1;
+// static const uint8_t MOTION_ADDR = 0x19 << 1;
 // static const uint8_t DISPLAY_ADDR = 0x3C << 1;
-//static const uint8_t TEMP_ADDR = 0x49 << 1;
+// static const uint8_t TEMP_ADDR = 0x49 << 1;
+// static const uint8_t BMP280_ADDR 0x77 << 1
+// static const uint8_t TEMP_HUMIDITY_ADDR 0x38 << 1
 
 
 /* USER CODE END PV */
